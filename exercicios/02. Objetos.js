8758b82c2129d58becec74d64c6fd98cfd06dabd
felipem5552 <felipemendes5552@gmail.com>
2025-08-22 00:32:12 -0300

    Create 02. Objetos.js
    Entender o princípio de Objetos

---
+++ exercicios/02. Objetos.js
@@ -0,0 +1,7 @@
+var rotina = ["acordar", "escovar os dentes", "tomar café da manhã", "estudar"]
+
+// como mostrar a rotina separadamente (de 0 à 3):
+console.log(rotina[0]) // acordar
+console.log(rotina[1]) // escovar os dentes
+console.log(rotina[2]) // tomar café da manhã
+console.log(rotina[3]) // estudar
