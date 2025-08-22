b7ac5b52fc035ada9eba1554e1a84c10ff631643
felipem5552 <felipemendes5552@gmail.com>
2025-08-22 00:47:02 -0300

    Entender a sintaxe e o fundamento de Variáveis

---
+++ exercicios/02. Variáveis (Var-Let-Const).js
@@ -0,0 +1,20 @@
+// 1️⃣ Tipos das Variáveis (var, let, const)
+
+// 1. Crie uma variável nome com seu nome e exiba no console
+var nome = "Felipe"
+console.log("Nome:", nome)
+
+// 2. Crie uma constante idade com sua idade e tente alterar o valor
+const idade = 25;
+console.log("Idade inicial:", idade)
+// idade = 26 // ❌ Isso dará um erro, pois uma varíavel const não pode ser alterada.
+
+// 3. Crie duas variáveis, a e b, atribua números à elas e faça a soma
+let a = 10
+let b = 15
+let soma = a + b
+console.log("Soma de a + b:", soma)
+
+// 4. Crie uma variável booleana estudando e exiba seu valor
+let estudando = true
+console.log("Estudando JavaScript?", estudando)
