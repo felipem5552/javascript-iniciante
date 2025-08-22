fda0102270f41b910bf7dbf52ba6154bede5382d
felipem5552 <felipemendes5552@gmail.com>
2025-08-22 00:56:34 -0300

    Entender a sintaxe e o fundamento de Null e Undefined

---
+++ exercicios/03. Null e Undefined.js
@@ -0,0 +1,18 @@
+// 1️⃣ Conceitos básicos
+
+//OBS: null é um valor que você atribui de propósito para dizer que a variável está vazia, ou seja, algo está NULO.
+//OBS: undefined é quando uma variável existe, mas ainda não recebeu nenhum valor, ou seja, não foi definido.
+
+// 1. Variável sem valor
+let idade;
+console.log("Variável sem valor:", idade); 
+// undefined → não foi atribuído nenhum valor
+
+// 2. Atribuindo null
+idade = null;
+console.log("Variável com null:", idade); 
+// null → significa "nenhum valor", diferente de undefined
+
+
+//OBS: undefined → NaN (Not a Number)
+//OBS: null → 0
