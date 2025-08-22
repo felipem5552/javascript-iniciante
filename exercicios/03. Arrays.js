7f753796ac0a0239780c1520bb0f856094fc1751
felipem5552 <felipemendes5552@gmail.com>
2025-08-22 00:37:55 -0300

    Entender a sintaxe e o fundamento de Arrays

---
+++ exercicios/03. Arrays.js
@@ -0,0 +1,13 @@
+// 📌 Atividade: Rotina diária com horários
+
+var rotina = [
+  { atividade: "acordar", horario: "06:00" },
+  { atividade: "escovar os dentes", horario: "06:10" },
+  { atividade: "trocar de roupa", horario: "06:15" },
+  { atividade: "tomar café da manhã", horario: "06:30" },
+  { atividade: "estudar", horario: "07:00" }
+]
+
+
+// Consultar somente o horário de uma atividade específica
+console.log(rotina[3].horario) // 06:30
