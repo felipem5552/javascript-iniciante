320218eae02153a38dcb9570164c766f846dd6fc
felipem5552 <felipemendes5552@gmail.com>
2025-08-26 02:18:14 -0300

    Entender o fundamento de Else If

--- logica-de-programacao/exercicios/06.2 (Else If).js
+++ logica-de-programacao/exercicios/06.2 (Else If).js
@@ -1,17 +1,17 @@
 // 3️⃣ Verificação de Nota Anual (If, Else If e Else)
 
-//Neste exercício, iremos utilizar mais condições com if e else, porém utilizando diversas condições com o else if.
-//Else if → "senão se": ou seja, se a condição anterior do seu If não for verdadeira, ele verifica uma nova condição.
-//Else → é apenas para quando nenhuma das condições anteriores do If e do Else If for verdadeira, sendo uma espécie de "Solução Final" caso nenhuma das anteriores for a correta.
+// Neste exercício, iremos utilizar mais condições com if e else, porém utilizando diversas condições com o else if.
+// Else if → "senão se": ou seja, se a condição anterior do seu If não for verdadeira, ele verifica uma nova condição.
+// Else → é apenas para quando nenhuma das condições anteriores do If e do Else If for verdadeira, sendo uma espécie de "Solução Final" caso nenhuma das anteriores for a correta.
 
 // 1. Solicitar a nota anual do usuário e guarde esta informação dentro da variável nota;
 let nota = parseFloat(prompt("Digite aqui sua nota anual:"))
 
 // 2. Verificar o resultado da nota
-//Condições - Nota 10 → "Perfeito"
-//Nota de 7 a 9 → "Aprovado"
-//Nota de 5 a 6 → "Recuperação"
-//Nota abaixo de 5 → "Reprovado"
+// Condições - Nota 10 → "Perfeito"
+// Nota de 7 a 9 → "Aprovado"
+// Nota de 5 a 6 → "Recuperação"
+// Nota abaixo de 5 → "Reprovado"
 if (nota === 10) {
   alert("Sua nota foi perfeita!")
 } else if (nota >= 7) {
