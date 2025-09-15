5d96d3dfcbf82f088ee8e0d490d2b2e0fe50f300
Felipem552 <felipemendes5552@gmail.com>
2025-09-15 11:18:24 -0300

    Declare três variáveis e atribua valores numéricos a elas. Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.

---
+++ curso-bolsa-futuro-digital/logica-de-programacao/exercicios/revisao-200-exercicios-javascript/exercicio-07.js
@@ -0,0 +1,12 @@
+// Exercício 7: Declare três variáveis e atribua valores numéricos a elas. 
+// Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.
+
+let a = 10
+let b = '10' //o 10 aqui é uma string, e não um valor
+let c = 5
+
+console.log(a == b)      
+console.log(a === b) //O valor dos dois são iguais a 10. POrém um é Number, e o outro é String. Por isso não são estritamente iguais    
+console.log(a != b)       
+console.log(a > c)       
+console.log(c <= a)       
