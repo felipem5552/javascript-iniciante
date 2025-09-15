102ec0fe9bc9de44eb8d1a55449366222f39a80a
Felipem552 <felipemendes5552@gmail.com>
2025-09-15 10:06:08 -0300

    Declare uma variável chamada númeroMáximo, atribua a ela o valor máximo que um número pode ter em JavaScript e imprima o valor na console.

---
+++ curso-bolsa-futuro-digital/logica-de-programacao/exercicios/revisao-200-exercicios-javascript/exercicio-03.js
@@ -0,0 +1,13 @@
+//Exercício 3: Declare uma variável chamada "númeroMáximo", 
+// atribua a ela o valor máximo que um número pode ter em JavaScript e imprima o valor na console.
+
+// Representa o maior valor inteiro seguro no JavaScript 
+const numeromaximo = Number.MAX_SAFE_INTEGER 
+
+alert (`Curiosidade do Dia!
+
+Você sabia que o maior valor inteiro seguro no JavaScript é utilizando o Number.MAX_SAFE_INTEGER ?
+
+Ao rodar esta constante você obterá o seguinte valor: 
+                                
+                            ${numeromaximo}`)
