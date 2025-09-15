b56b3be051ef9c262596d45ba2182cd9ee3b20e1
Felipem552 <felipemendes5552@gmail.com>
2025-09-15 11:59:46 -0300

    Exercício 9: Escreva um programa que recebe um número e verifica se ele é positivo, negativo ou zero.

--- curso-bolsa-futuro-digital/logica-de-programacao/exercicios/revisao-200-exercicios-javascript/exercicio-09.js
+++ curso-bolsa-futuro-digital/logica-de-programacao/exercicios/revisao-200-exercicios-javascript/exercicio-09.js
@@ -1,12 +1,24 @@
-//Exercício 9: Escreva um programa que recebe um número e verifica se ele é positivo, negativo ou zero.
+//Exercício 10: Escreva um programa que recebe duas notas de um aluno, calcula a média e 
+// imprime se o aluno foi aprovado ou reprovado (considerando que a média para aprovação é 7).
 
-let num = parseInt(prompt("Digite um número de sua preferência, e direi se ele é positivo, negativo ou zero:"))
+let n1 = parseFloat(prompt(`Consultor de Médias Escolares
+Olá aluno! Digite sua primeira nota:
 
-if (num > 0) {
-    alert(`O número ${num} é positivo`)
+1° Nota = Aguardando...
+2° Nota = Aguardando...
+`))
+
+let n2 = parseFloat(prompt(`Consultor de Médias Escolares
+Olá aluno! Digite sua segunda nota:
+
+1° Nota = ${n1}
+2° Nota = Aguardando...
+`))
+
+let media = (n1 + n2) / 2
+
+if (media => 7) {
+    alert(`Parabéns, você foi aprovado com ${media}`)
 }
-else if (num < 0) {
-    alert(`O número ${num} é negativo`)
-}
-else 
-    alert(`O número ${num} é zero`)
+else
+    alert (`Infelizmente você foi reprovado :( A média para passar é 7)`)
