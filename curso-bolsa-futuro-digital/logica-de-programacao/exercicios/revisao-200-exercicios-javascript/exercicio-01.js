b1649edb26f9a7af94cc201c7f8f54a3ba7f3e7d
Felipem552 <felipemendes5552@gmail.com>
2025-09-15 09:53:37 -0300

    Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores na console.

---
+++ curso-bolsa-futuro-digital/logica-de-programacao/exercicios/revisao-200-exercicios-javascript/exercicio-01.js
@@ -0,0 +1,14 @@
+//Exercício 1: Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores na console.
+
+var idade = parseInt(prompt(`Coletor de Dados 👶: 
+
+Bem-Vindo! Por favor comece informando sua idade:`)) || 0
+
+var peso = parseInt(prompt(`Coletor de Dados 🏋️: 
+
+Agora digite seu peso:`)) || 0
+
+
+alert(`Informações salvas com sucesso!
+Sua idade é: ${idade}
+Seu peso é: ${peso}`)
