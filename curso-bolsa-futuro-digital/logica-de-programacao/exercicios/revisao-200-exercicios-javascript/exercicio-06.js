958595cfcb2a5dd72f522a86c798540c1fd98a71
Felipem552 <felipemendes5552@gmail.com>
2025-09-15 10:47:32 -0300

    Declare duas variáveis e atribua valores booleanos a elas. Use os operadores lógicos AND, OR e NOT para realizar operações lógicas entre as variáveis. Imprima os resultados.

---
+++ curso-bolsa-futuro-digital/logica-de-programacao/exercicios/revisao-200-exercicios-javascript/exercicio-06.js
@@ -0,0 +1,18 @@
+// Exercício 6: Declare duas variáveis e atribua valores booleanos a elas. 
+// Use os operadores lógicos AND, OR e NOT para realizar operações lógicas entre as variáveis. 
+// Imprima os resultados.
+
+let a = true
+let b = false
+
+//Aplicar operadores lógicos
+let resultadoAND = a && b
+let resultadoOR = a || b
+let resultadoNOTA = !a
+let resultadoNOTB = !b
+
+alert(`Resultados das operações lógicas:
+a AND b: ${resultadoAND}
+a OR b: ${resultadoOR}
+NOT a: ${resultadoNOTA}
+NOT b: ${resultadoNOTB}`)
