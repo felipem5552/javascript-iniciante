31cec232cfb0c3a207c7e68b364961576e82a674
Felipem552 <felipemendes5552@gmail.com>
2025-09-15 10:24:17 -0300

    Declare duas variáveis chamadas nome e sobrenome, atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada nomeCompleto e imprima.

---
+++ curso-bolsa-futuro-digital/logica-de-programacao/exercicios/revisao-200-exercicios-javascript/exercicio-04.js
@@ -0,0 +1,8 @@
+//Exercício 4: Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. 
+// Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
+
+let nome = prompt(" 1️⃣  Bem-Vindo! Por favor, digite seu primeiro nome:")
+let sobrenome = prompt(" 2️⃣  Agora digite seu sobrenome:")
+let nomeCompleto = nome + " " + sobrenome
+
+alert (`👋 Olá ${nomeCompleto}`)
