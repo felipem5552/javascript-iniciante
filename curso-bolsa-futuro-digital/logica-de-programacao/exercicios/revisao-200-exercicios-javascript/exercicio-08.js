c96d89851010b1a4e8475d04e9e28fedb6fcf2be
Felipem552 <felipemendes5552@gmail.com>
2025-09-15 11:42:00 -0300

    Declare uma variável e atribua um valor numérico a ela. Use o operador de incremento para aumentar o valor da variável. Imprima o resultado.

---
+++ curso-bolsa-futuro-digital/logica-de-programacao/exercicios/revisao-200-exercicios-javascript/exercicio-08.js
@@ -0,0 +1,5 @@
+//Exercício 8: Declare uma variável e atribua um valor numérico a ela. 
+// Use o operador de incremento para aumentar o valor da variável. Imprima o resultado.
+
+let num = parseInt(prompt("Digite um número de sua preferência e irei incrementar o valor com +1:"));
+alert(++num); 
