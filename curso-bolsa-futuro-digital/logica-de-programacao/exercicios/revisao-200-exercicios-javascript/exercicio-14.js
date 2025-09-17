6dee8c059de860722c3956ee26713e58bf893c32
felipem5552 <felipemendes5552@gmail.com>
2025-09-17 20:54:23 -0300

    Escreva um programa que verifica se um ano é bissexto.

--- curso-bolsa-futuro-digital/logica-de-programacao/exercicios/revisao-200-exercicios-javascript/exercicio-14.js
+++ curso-bolsa-futuro-digital/logica-de-programacao/exercicios/revisao-200-exercicios-javascript/exercicio-14.js
@@ -3,18 +3,15 @@
 let ano = parseInt(prompt("Digite um ano para verificar se ele é bissexto:"))
 
 if (ano % 400 === 0) {
-   alert(ano + " é um ano bissexto!");
+   alert(ano + " é um ano bissexto!")
 } 
-
 else if (ano % 100 === 0) {
-    alert(ano + " não é um ano bissexto!");
+    alert(ano + " não é um ano bissexto!")
 } 
-
 else if (ano % 4 === 0) {
-    alert(ano + " é um ano bissexto!");
+    alert(ano + " é um ano bissexto!")
 } 
-
 else {
-    alert(ano + " não é um ano bissexto!");
+    alert(ano + " não é um ano bissexto!")
 }
 
