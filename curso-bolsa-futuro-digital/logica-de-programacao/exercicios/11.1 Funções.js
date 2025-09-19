ce493070a6eb6ba92fdcc4f20eb95958dae917c4
felipem5552 <felipemendes5552@gmail.com>
2025-09-18 23:41:55 -0300

    Entender a sintaxe e o fundamento de Funções

---
+++ curso-bolsa-futuro-digital/logica-de-programacao/exercicios/11.1 Funções.js
@@ -0,0 +1,16 @@
+// 6️⃣ Funções - Exemplo básico
+
+// Neste exercício, vamos aprender como criar e chamar uma função simples em JavaScript.
+
+// 1. Escreva uma função em JavaScript que mostre na tela a mensagem “Bom dia, Mundo!!!”
+function helloworld() {
+    alert("Bom dia, Mundo!!!")
+}
+
+helloworld()
+
+// 2. Observações importantes:
+// - A palavra-chave "function" é usada para declarar uma função em JavaScript.
+// - O nome da função deve ser descritivo e seguir boas práticas.
+// - Para executar a função, é necessário chamá-la pelo nome seguido de parênteses, como: helloworld()
+
