e5fa717e4a428f4a8d878ddb9293a82d3c297fe9
unknown <felipemendes5552@gmail.com>
2025-09-23 00:30:07 -0300

    Verifique se um número está dentro de um intervalo.

---
+++ curso-bolsa-futuro-digital/logica-de-programacao/exercicios/revisao-200-exercicios-javascript/exercicio-19.js
@@ -0,0 +1,9 @@
+//Exercício 19: Verifique se um número está dentro de um intervalo.
+
+let n = parseFloat(prompt("Digite um número para verificar se está dentro do intervalo:")) 
+
+if (n >= 10 && n <= 20) {
+  alert("Número dentro do intervalo");
+} else {
+  alert("Número fora do intervalo");
+}
