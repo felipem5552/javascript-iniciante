933ebb2de60c401578add293cc7982b8ea338e7a
unknown <felipemendes5552@gmail.com>
2025-10-08 19:16:59 -0300

    Removendo pastas desnecessárias

--- curso-bolsa-futuro-digital/01-logica-de-programacao/exercicios/revisao-200-exercicios-javascript/exercicio-02.js
+++
@@ -1,5 +0,0 @@
-//Exercício 2: Declare uma constante chamada "PI", atribua o valor de Pi a ela e imprima o valor na console.
-
-const PI = 3.14
-
-alert(`O valor de Pi é: ${PI}`)
--- curso-bolsa-futuro-digital/01-logica-de-programacao/exercicios/revisao-200-exercicios-javascript/exercicio-27.js
+++
@@ -1,4 +0,0 @@
-for (let n = 1; n <= 10; n++){
-    
-    console.log(`5 x ${n} = ${n * 5}`)
-}
