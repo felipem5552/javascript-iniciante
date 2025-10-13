8b828487c5cc9a8745df1371700220ae4930c979
felipem5552 <felipemendes5552@gmail.com>
2025-10-13 11:45:45 -0300

    Create POO - hierarquia animal.ts

---
+++ curso-bolsa-futuro-digital/05-orientacao-a-objetos/POO - hierarquia animal.ts
@@ -0,0 +1,13 @@
+//Crie uma hierarquia de classes com Animal sendo a classe base e Gato e Pássaro como subclasses
+//Implemente métodos específicos para cada animal.
+
+interface Animal {
+    nome: string
+    emitirsom(): void
+}
+
+class Gato implements Animal {
+    this.name = name
+
+
+}
