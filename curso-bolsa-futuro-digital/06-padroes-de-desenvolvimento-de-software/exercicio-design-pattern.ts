6c708159afc119035e9f10fce64e4edbe86d63bb
Felipe Mendes Alves <felipemendes5552@gmail.com>
2025-11-03 23:46:15 -0300

    Refactor page interface and implement new page classes

--- curso-bolsa-futuro-digital/06-padroes-de-desenvolvimento-de-software/exercicio-design-pattern.ts
+++ curso-bolsa-futuro-digital/06-padroes-de-desenvolvimento-de-software/exercicio-design-pattern.ts
@@ -1,16 +1,13 @@
-// Produto abstrato (o que precisa acontecer no final de tudo = gerar uma lista)
 interface Pagina {
-    lista(): void
+    exibir(): void
 }
-
-
-// Produtos concretos (onde vai ser criado os elementos necessários que precisam estar dentro da lista solicitada)
 
 // Elementos necessários para Currículos
 class PaginaHabilidades implements Pagina {
     exibir(): void {
         console.log("Página de Habilidades")
     }
+}
 
 class PaginaEducacao implements Pagina {
     exibir(): void {
@@ -31,16 +28,77 @@ class PaginaIntroducao implements Pagina {
     }
 }
 
-class PaginaIntroducao implements Pagina {
+class PaginaResultados implements Pagina {
     exibir(): void {
-        console.log("Página de Resultado")
+        console.log("Página de Resultados")
+    }
+}
+
+class PaginaConclusao implements Pagina {
+    exibir(): void {
+        console.log("Página de Conclusão")
+    }
+}
+
+class PaginaResumo implements Pagina {
+    exibir(): void {
+        console.log("Página de Resumo")
+    }
+}
+
+class PaginaBibliografia implements Pagina {
+    exibir(): void {
+        console.log("Página de Bibliografia")
+    }
+}
+
+// Factory Metod
+abstract class Documento {
+    protected paginas: Pagina[] = []
+
+    abstract criarPaginas(): void
+
+    public mostrarPaginas(): void {
+        console.log(`${this.constructor.name} -------`)
+        this.paginas.forEach(p => p.exibir())
+    }
+}
+
+class Curriculo extends Documento {
+    criarPaginas(): void {
+        this.paginas.push(
+            new PaginaHabilidades(),
+            new PaginaEducacao(),
+            new PaginaExperiencia()
+        )
+    }
+}
+
+class Relatorio extends Documento {
+    criarPaginas(): void {
+        this.paginas.push(
+            new PaginaIntroducao(),
+            new PaginaResultados(),
+            new PaginaConclusao(),
+            new PaginaResumo(),
+            new PaginaBibliografia()
+        )
     }
 }
 
 
+function main() {
+    // Criando e exibindo o Currículo
+    const curriculo = new Curriculo()
+    curriculo.criarPaginas()
+    curriculo.mostrarPaginas()
 
+    console.log("")
 
-
-
-
+    // Criando e exibindo o Relatório
+    const relatorio = new Relatorio()
+    relatorio.criarPaginas()
+    relatorio.mostrarPaginas()
 }
+
+main()
